21cfa26da01c80b20d3068dd7bdc73b1ebc930aa
Soundararajan <subashin@0c4de9a1e09a.ant.amazon.com>
2015-04-16 02:04:37 -0700

    Initial commit

---
+++ src/main/java/com/example/AgendaToday.java
@@ -0,0 +1,80 @@
+package com.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import java.io.IOException;
+import java.util.*;
+
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.Events;
+import com.google.api.services.calendar.model.Event;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("agendatoday")
+public class AgendaToday {
+	private static final String APPLICATION_NAME = "CalendarFeed";
+
+	/** Global instance of the HTTP transport. */
+	private static HttpTransport httpTransport;
+
+	/** Global instance of the JSON factory. */
+	private static final JsonFactory JSON_FACTORY = JacksonFactory
+			.getDefaultInstance();
+
+	private static final String CALENDAR_ID = "subashini.echo.hack@gmail.com";
+
+	private static Calendar client;
+
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent to
+	 * the client as "text/plain" media type.
+	 * 
+	 * @return String that will be returned as a text/plain response.
+	 */
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String get() {
+		String result = "";
+		try {
+			try {
+				com.google.api.client.util.DateTime currentTime = new com.google.api.client.util.DateTime(
+						new Date(), TimeZone.getDefault());
+				httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+
+				CalendarAuthenticator authenticator = new CalendarAuthenticator();
+				GoogleCredential credential = authenticator.getCredential();
+
+				credential.refreshToken();
+
+				client = new Calendar.Builder(httpTransport, JSON_FACTORY,
+						credential).setApplicationName(APPLICATION_NAME)
+						.build();
+
+				Events events = client.events().list(CALENDAR_ID)
+						.setTimeMin(currentTime).execute();
+
+				for (Event event : events.getItems()) {
+					result = result + "\n" + event.getSummary() + " from "
+							+ event.getStart().getDateTime()
+							+ " till " + event.getEnd().getDateTime();
+				}
+			} catch (IOException e) {
+				System.err.println(e.getMessage());
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+		return result;
+	}
+
+}
---
+++ src/main/java/com/example/CalendarAuthenticator.java
@@ -0,0 +1,58 @@
+package com.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.calendar.CalendarScopes;
+
+public class CalendarAuthenticator {
+	private GoogleCredential credential;
+
+	/** Global instance of the JSON factory. */
+	private static final JsonFactory JSON_FACTORY = JacksonFactory
+			.getDefaultInstance();
+
+	/** E-mail address of the service account. */
+	private static final String SERVICE_ACCOUNT_EMAIL = "918593848967-1f5evctnfp865gavbhq9g0nvuivuq6sn@developer.gserviceaccount.com";
+
+	private static final String FILE_NAME = "./resources/Echo Hack-2bc576fb05e1.p12";
+
+	public CalendarAuthenticator() {
+		setCredential();
+	}
+
+	public void setCredential() {
+		HttpTransport httpTransport;
+		try {
+			httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+
+			this.credential = new GoogleCredential.Builder()
+					.setTransport(httpTransport)
+					.setJsonFactory(JSON_FACTORY)
+					.setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
+					.setServiceAccountScopes(Collections.singleton(CalendarScopes.CALENDAR))
+					.setServiceAccountPrivateKeyFromP12File(new File(FILE_NAME))
+					.build();
+
+		} catch (GeneralSecurityException e) {
+			throw new RuntimeException(
+					"Could not create google oauth credential\n Caused By:"
+							+ e.getMessage());
+		} catch (IOException e) {
+			throw new RuntimeException(
+					"Could not create google oauth credential\n Caused By:"
+							+ e.getMessage());
+		}
+	}
+
+	public GoogleCredential getCredential() {
+		return this.credential;
+	}
+}
